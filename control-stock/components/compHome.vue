50fff2b177b1a6a997beea702c93329101d2f446
Patrick Covre Rodrigues <112516755+RodriguesCovre@users.noreply.github.com>
2024-08-08 20:28:18 -0300

    Centralizando textos

--- control-stock/components/compHome.vue
+++ control-stock/components/compHome.vue
@@ -8,16 +8,16 @@
             </h1>
             <div class="mt-8">
                 <div class="flex justify-evenly align-middle">
-                    <div class="w-[242px] mx-8 h-44 border-solid border-2 border-indigo-600">Gestão de Usuário</div>
-                    <div class="w-[242px] mx-8 border-solid border-2 border-indigo-600">Cadastro de Fornecedor</div>
-                    <div class="w-[242px] mx-8 border-solid border-2 border-indigo-600">Cadastro de Produtos</div>
+                    <div class="w-[242px] mx-8 h-44 border-solid border-2 border-indigo-600 text-center"><h1 class="mt-24">Gestão de Usuário</h1></div>
+                    <div class="w-[242px] mx-8 border-solid border-2 border-indigo-600 text-center"><h1 class="mt-24">Cadastro de Fornecedor</h1></div>
+                    <div class="w-[242px] mx-8 border-solid border-2 border-indigo-600 text-center"><h1 class="mt-24">Cadastro de Produtos</h1></div>
                 </div>
                 <div class="flex justify-evenly align-middle my-8">
-                    <div class="w-[242px] mx-8 h-44 border-solid border-2 border-indigo-600">Saída de Estoque</div>
-                    <div class="w-[242px] mx-8 border-solid border-2 border-indigo-600">Entrada do Estoque</div>
-                    <div class="w-[242px] mx-8 border-solid border-2 border-indigo-600">Gráficos</div>
+                    <div class="w-[242px] mx-8 h-44 border-solid border-2 border-indigo-600 text-center"><h1 class="mt-24">Saída de Estoque</h1></div>
+                    <div class="w-[242px] mx-8 border-solid border-2 border-indigo-600 text-center"><h1 class="mt-24">Entrada do Estoque</h1></div>
+                    <div class="w-[242px] mx-8 border-solid border-2 border-indigo-600 text-center"><h1 class="mt-24">Gráficos</h1></div>
                 </div>
-                <div class="w-[242px] h-44 mx-8 border-solid border-2 border-indigo-600">Cadastro de clientes</div>
+                <div class="w-[242px] h-44 mx-8 border-solid border-2 border-indigo-600 text-center"><h1 class="mt-24">Cadastro de clientes</h1></div>
             </div>
         </div>
     </div>
